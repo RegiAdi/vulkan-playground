66427d41c025751adc8dd41060680b652a31ea60
Regi Adi <regiadi.priyanto@gmail.com>
2024-05-19 01:06:10 +0700

    Add GLFW library

--- src/main.c
+++ src/main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <cglm/cglm.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char **argv)
 {
 	printf("Hello there.\n");
+
+	GLFWwindow *window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
 }
